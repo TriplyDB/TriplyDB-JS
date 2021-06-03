c09ac07048a3eb7fa9cbbcbdb93cba46e81f8043
Laurens Rietveld <laurens.rietveld@gmail.com>
2021-06-03 22:26:47 +0200

    Expose toFile compression method on member function for easier use

--- src/utils/AsyncIteratorHelperWithToFile.ts
+++ src/utils/AsyncIteratorHelperWithToFile.ts
@@ -1,17 +1,13 @@
 import * as fs from "fs-extra";
 import AsyncIteratorHelper, { AsyncConfig } from "./AsyncIteratorHelper";
 import zlib from "zlib";
-export interface AsyncConfigWithToFile<ResultType, OutputType> extends AsyncConfig<ResultType, OutputType> {
-  compressed?: boolean;
-}
+
 export default class AsyncIteratorHelperWithToFile<ResultType, OutputClass> extends AsyncIteratorHelper<
   ResultType,
   OutputClass
 > {
-  private compressed = false;
-  constructor(config: AsyncConfigWithToFile<ResultType, OutputClass>) {
+  constructor(config: AsyncConfig<ResultType, OutputClass>) {
     super(config);
-    if (config.compressed !== undefined) this.compressed = config.compressed;
   }
   private compress(data: string) {
     return new Promise<Buffer>((resolve, reject) => {
@@ -21,12 +17,12 @@ export default class AsyncIteratorHelperWithToFile<ResultType, OutputClass> exte
       });
     });
   }
-  public async toFile(filePath: string) {
+  public async toFile(filePath: string, opts?: { compressed?: boolean }) {
     const f = await fs.open(filePath, "w");
     let results: ResultType[] | void;
     while ((results = await this["_getNextPage"]())) {
       if (results && results.length && this["_page"]) {
-        if (this.compressed) {
+        if (opts?.compressed) {
           await fs.write(f, this.compress(this["_page"]));
         } else {
           await fs.write(f, this["_page"]);
