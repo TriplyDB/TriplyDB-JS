ce6535fc80e10852ccaa5bd7efef5b7203611cf3
Laurens Rietveld <laurens.rietveld@triply.cc>
2023-04-18 11:49:39 +0200

    Added comment to rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,3 +1,5 @@
+// We need rollup, because `pkg` (used for building executables) does not support ESM yet.
+// See here for the status: https://github.com/vercel/pkg/issues/1291
 import sucrase from "@rollup/plugin-sucrase";
 import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
